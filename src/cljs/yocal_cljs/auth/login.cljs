899527931beadcdf494d327ee7c8bb9989613e71
Kotokz <Kotokz@live.com>
2015-08-31 17:04:27 +0800

    Update login.cljs

--- src/cljs/yocal_cljs/auth/login.cljs
+++ src/cljs/yocal_cljs/auth/login.cljs
@@ -1,26 +1,31 @@
 (ns yocal-cljs.auth.login
   (:require [reagent.core :as ratom]
+            [re-frame.core :as re-frame]
             [reforms.reagent :include-macros true :as f]
             [reforms.validation :as v :include-macros true]))
 
 (def auth-validators
   [(v/present [:username] "Enter login name")
    (v/present [:password] "Enter password")])
+
 (def data (ratom/atom {}))
+(def credentials (ratom/atom {:username "" :password ""}))
 
 (defn sign-in!
  [data credentials]
  (when (apply v/validate! data credentials auth-validators)
-   #(pr credentials)))
+   (re-frame/dispatch [:login @data])))
 
 (defn login-form-view
-  [credentials]
-    (v/form credentials
+  []
+   (let [jwt @(re-frame/subscribe [:auth-jwt])]
+    (v/form credentials {:key "form"}
       (f/group-title {:class "group-title-main" :key "title"} "Please login with your user name")
       (v/text {:key "login"} "Login" data [:username] :placeholder "Enter login")
       (v/password {:key "pwd"} "Password" data [:password] :placeholder "Enter password")
       (f/form-buttons
-        (f/button {:key "save"} "Save" #(sign-in! data credentials)))))
+        (f/button {:key "save"} "Save" #(sign-in! data credentials))
+        (f/button {:key "get"} "Get" #(re-frame/dispatch [:get-balance jwt]))))))
 
 
 (f/set-options!
